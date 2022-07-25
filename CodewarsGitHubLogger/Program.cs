f032dfbb0ca7a01807162240df8bd9381b20ed06
Zehan Khan <73664886+zehan12@users.noreply.github.com>
2022-07-25 17:02:03 +0530

    Initial commit

---
+++ CodewarsGitHubLogger/Program.cs
@@ -0,0 +1,328 @@
+/*
+Program created by José De Freitas (https://github.com/JoseDeFreitas).
+Link to the repository: https://github.com/JoseDeFreitas/CodewarsGitHubLogger.
+Licensed under the BSD-2-Clause License.
+*/
+
+using System;
+using System.IO;
+using OpenQA.Selenium;
+using System.Net.Http;
+using System.Text.Json;
+using OpenQA.Selenium.Firefox;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace CodewarsGitHubLogger
+{
+    class Program
+    {
+        static HttpClient httpClient = new HttpClient();
+        static int numberOfExceptions = 0;
+        static List<string> idsOfExceptions = new List<string>();
+        static Dictionary<string, string> languagesExtensions = new Dictionary<string, string>() {
+            {"agda", "agda"}, {"bf", "b"}, {"c", "c"}, {"cmlf", "cmfl"},
+            {"clojure", "clj"}, {"cobol", "cob"}, {"coffeescript", "coffee"}, {"commonlisp", "lisp"},
+            {"coq", "coq"}, {"cplusplus", "cpp"}, {"crystal", "cr"}, {"csharp", "cs"},
+            {"dart", "dart"}, {"elixir", "ex"}, {"elm", "elm"}, {"erlang", "erl"},
+            {"factor", "factor"}, {"forth", "fth"}, {"fortran", "f"}, {"fsharp", "fs"},
+            {"go", "go"}, {"groovy", "groovy"}, {"haskell", "hs"}, {"haxe", "hx"},
+            {"idris", "idr"}, {"java", "java"}, {"javascript", "js"}, {"julia", "jl"},
+            {"kotlin", "kt"}, {"lean", "lean"}, {"lua", "lua"}, {"nasm", "nasm"},
+            {"nimrod", "nim"}, {"objective", "m"}, {"ocaml", "ml"}, {"pascal", "pas"},
+            {"perl", "pl"}, {"php", "php"}, {"powershell", "ps1"}, {"prolog", "pro"},
+            {"purescript", "purs"}, {"python", "py"}, {"r", "r"}, {"racket", "rkt"},
+            {"ruby", "rb"}, {"rust", "rs"}, {"scala", "scala"}, {"shell", "sh"},
+            {"sql", "sql"}, {"swift", "swift"}, {"typescript", "ts"}, {"vb", "vb"},
+        };
+        static Dictionary<string, List<string>> kataCategories = new Dictionary<string, List<string>>() {
+            {"reference", new List<string>()}, // Equivalent to the "Fundamentals" category
+            {"algorithms", new List<string>()},
+            {"bug_fixes", new List<string>()},
+            {"refactoring", new List<string>()},
+            {"games", new List<string>()} // Equivalent to the "Puzzles" category
+        };
+
+        static async Task Main(string[] args)
+        {
+            FirefoxOptions options = new FirefoxOptions();
+            options.AddArgument("--headless");
+            IWebDriver driver = new FirefoxDriver("./", options);
+
+            // Define variables for each credential
+            string codewarsUsername = "";
+            string githubUsername = "";
+            string githubPassword = "";
+
+            try
+            {
+                codewarsUsername = args[0];
+                githubUsername = args[1];
+                githubPassword = args[2];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("You must pass the credentials.");
+                Environment.Exit(1);
+            }
+
+            string completedKatasUrl = $"https://www.codewars.com/api/v1/users/{codewarsUsername}/code-challenges/completed";
+            string kataInfoUrl = "https://www.codewars.com/api/v1/code-challenges/";
+            string mainFolderPath = "../Katas";
+
+            Directory.CreateDirectory(mainFolderPath);
+
+            SignInToCodewars(driver, githubUsername, githubPassword);
+
+            // Response used only to get the total number of pages available
+            Stream mainResponseJson = await httpClient.GetStreamAsync(completedKatasUrl);
+            KataCompleted mainResponseObject = await JsonSerializer.DeserializeAsync<KataCompleted>(mainResponseJson);
+            int numberOfPages = mainResponseObject.totalPages;
+
+            for (int page = 0; page < numberOfPages; page++)
+            {
+                // Response used to get the information of all the katas in the specified page
+                Stream responseStream = await httpClient.GetStreamAsync($"{completedKatasUrl}?page={page}");
+                KataCompleted kataObject = await JsonSerializer.DeserializeAsync<KataCompleted>(responseStream);
+
+                foreach (var kata in kataObject.data)
+                {
+                    // Response used only to get the description of the kata
+                    Stream responseKataInfo = await httpClient.GetStreamAsync($"{kataInfoUrl}{kata.id}");
+                    KataInfo kataInfoObject = await JsonSerializer.DeserializeAsync<KataInfo>(responseKataInfo);
+                    string kataFolderPath = Path.Combine(mainFolderPath, kata.slug);
+
+                    kataCategories[kataInfoObject.category].Add($"- [{kata.name}](./Katas/{kata.slug})");
+
+                    await CreateMainFilesAsync(
+                        kataFolderPath, kata.name, kataInfoUrl,
+                        kata.id, kata.completedAt, kata.completedLanguages,
+                        kataInfoObject.description, kataInfoObject.rank, kataInfoObject.tags
+                    );
+
+                    foreach (string language in kata.completedLanguages)
+                    {
+                        string codeFilePath = Path.Combine(kataFolderPath, $"{kata.slug}.{languagesExtensions[language]}");
+
+                        await CreateCodeFileAsync(driver, codeFilePath, kata.id, language);
+                    }
+                }
+            }
+
+            try
+            {
+                if (args[3] == "-i" || args[3] == "--index")
+                    await CreateIndexFileAsync();
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("'Index' flag was not specified");
+            }
+
+            driver.Quit();
+
+            string separatorLine = "-------------------------\n";
+
+            if (numberOfExceptions == 0)
+                Console.WriteLine($"{separatorLine}All data was loaded successfully.");
+            else
+                Console.WriteLine($"{separatorLine}All data was loaded except {numberOfExceptions.ToString()} katas: {string.Join(" - ", idsOfExceptions)}.");
+        }
+
+        /// <summary>
+        /// Using the credentials provided (GitHub username and passwords) to sign in to
+        /// Codewars. It doesn't allow signing in with Codewars creadentials (username and
+        /// password) because it's intended to work only if the user registered using the
+        /// GitHub OAuth.
+        /// </summary>
+        /// <param name="driver">The Firefox driver initialised in the Main method.</param>
+        /// <param name="githubUsername">The GitHub username of the user.</param>
+        /// <param name="githubPassword">The GitHub password of the user.</param>
+        /// <exception>When the driver can't connect to the Codewars website.</exception>
+        static void SignInToCodewars(IWebDriver driver, string githubUsername, string githubPassword)
+        {
+            try
+            {
+                driver.Navigate().GoToUrl(@"https://www.codewars.com/users/sign_in");
+            }
+            catch (TimeoutException exception)
+            {
+                Console.WriteLine(exception);
+                Environment.Exit(2);
+            }
+
+            IWebElement siginForm = driver.FindElement(By.Id("new_user"));
+            siginForm.FindElement(By.TagName("button")).Click();
+
+            driver.FindElement(By.Id("login_field")).SendKeys(githubUsername);
+            driver.FindElement(By.Id("password")).SendKeys(githubPassword);
+            driver.FindElement(By.Name("commit")).Click();
+        }
+
+        /// <summary>
+        /// "Main files" are: the kata folder and the README.md file. The name of the
+        /// folder will be the slug of the kata, and the README.md file will contain some
+        /// information about the kata (the name, the link to the Codewars page of the kata,
+        /// the date of completion, the completed languages, and the description).
+        /// </summary>
+        /// <param name="folder">The name of the folder (based on the kata slug).</param>
+        /// <param name="name">The name of the kata (not the slug).</param>
+        /// <param name="url">The url to the kata on the Codewars website.</param>
+        /// <param name="id">The ID of the kata.</param>
+        /// <param name="date">The date the kata was completed on.</param>
+        /// <param name="languages">The programming languages the kata was completed in.</param>
+        /// <param name="description">The description of the kata (Markdown-formatted).</param>
+        /// <exception>If the folder can't be created.</exception>
+        static async Task CreateMainFilesAsync(
+            string folder, string name, string url,
+            string id, string date, List<string> languages,
+            string description, Dictionary<string, object> rank, List<string> tags
+        )
+        {
+            string filePath = Path.Combine(folder, "README.md");
+            string content =
+            $"# [{name}]({url}{id})\n"
+            + $"\n- **Completed at:** {date}\n\n"
+            + $"- **Completed languages:** {string.Join(", ", languages)}\n\n"
+            + $"- **Tags:** {string.Join(", ", tags)}\n\n"
+            + $"- **Rank:** {rank["name"]}\n\n"
+            + $"## Description\n\n{description}";
+
+            try
+            {
+                Directory.CreateDirectory(folder);
+
+                if (File.Exists(filePath))
+                {
+                    if (String.Compare(await File.ReadAllTextAsync(filePath), content) != 0)
+                        await File.WriteAllTextAsync(filePath, content);
+                    else
+                        return;
+                }
+                else
+                    await File.WriteAllTextAsync(filePath, content);
+            }
+            catch (IOException exception)
+            {
+                Console.WriteLine(exception);
+                numberOfExceptions++;
+                idsOfExceptions.Add(id);
+            }
+        }
+
+        /// <summary>
+        /// Pulls the code of the kata from the Codewars website. It supports all the
+        /// programming languages the code was completed in. With that code in hand,
+        /// copies it to a new file with the proper language extensions and adds it to
+        /// the kata folder. One file per programming language completed.
+        /// </summary>
+        /// <param name="driver">The Firefox driver initialised in the Main method.</param>
+        /// <param name="path">The path of the code file (for each language).</param>
+        /// <param name="id">The ID of the kata.</param>
+        /// <param name="language">The programming language inside the list of them.</param>
+        /// <exception>
+        /// When the driver can't connect to the Codewars website or the DOM elements
+        /// couldn't be found (due to a problem with Codewars).
+        /// </exception>
+        static async Task CreateCodeFileAsync(IWebDriver driver, string path, string id, string language)
+        {
+            IWebElement solutionsList;
+            IWebElement solutionItem;
+            string solutionCode = "";
+
+            try
+            {
+                driver.Navigate().GoToUrl($@"https://www.codewars.com/kata/{id}/solutions/{language}/me/newest");
+
+                solutionsList = driver.FindElement(By.Id("solutions_list"));
+                solutionItem = solutionsList.FindElement(By.TagName("li"));
+                solutionCode = solutionItem.FindElement(By.TagName("pre")).Text;
+
+                if (File.Exists(path))
+                {
+                    if (String.Compare(await File.ReadAllTextAsync(path), solutionCode) != 0)
+                        await File.WriteAllTextAsync(path, solutionCode);
+                    else
+                        return;
+                }
+                else
+                    await File.WriteAllTextAsync(path, solutionCode);
+            }
+            catch (TimeoutException exception)
+            {
+                Console.WriteLine(exception);
+                return;
+            }
+            catch (NoSuchElementException)
+            {
+                return;
+            }
+            catch (IOException)
+            {
+                numberOfExceptions++;
+                idsOfExceptions.Add(id);
+            }
+        }
+
+        /// <summary>
+        /// If the program was run with the --index flag, this method executes, and
+        /// creates a file called "INDEX.md" that lists all the katas based on its
+        /// category/discipline, in order to make the navigation through the katas
+        /// easier and faster, while being logic.
+        /// <summary>
+        /// <exception>If the file can't be created.</exception>
+        static async Task CreateIndexFileAsync()
+        {
+            string filePath = "../INDEX.md";
+            string content =
+            $"# Index of katas by its category/discipline\n\n"
+            + $"These are all the code-challenges I've successfully completed. Total completed katas: {{}}"
+            + $"\n## Fundamentals\n\n{string.Join("\n", kataCategories["reference"])}"
+            + $"\n## Algorithms\n\n{string.Join("\n", kataCategories["algorithms"])}"
+            + $"\n## Bug Fixes\n\n{string.Join("\n", kataCategories["bug_fixes"])}"
+            + $"\n## Refactoring\n\n{string.Join("\n", kataCategories["refactoring"])}"
+            + $"\n## Puzzles\n\n{string.Join("\n", kataCategories["games"])}";
+
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    if (String.Compare(await File.ReadAllTextAsync(filePath), content) != 0)
+                        await File.WriteAllTextAsync(filePath, content);
+                    else
+                        return;
+                }
+                else
+                    await File.WriteAllTextAsync(filePath, content);
+            }
+            catch (IOException exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+    }
+
+    class KataCompleted
+    {
+        public int totalPages { get; set; }
+        public int totalItems { get; set; }
+        public List<KataData> data { get; set; }
+    }
+
+    class KataData
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string slug { get; set; }
+        public string completedAt { get; set; }
+        public List<string> completedLanguages { get; set; }
+    }
+
+    class KataInfo
+    {
+        public string category { get; set; }
+        public string description { get; set; }
+        public List<string> tags { get; set; }
+        public Dictionary<string, object> rank { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,135 @@
+# CodewarsGitHubLogger
+
+This program allows you to connect to [Codewars](https://www.codewars.com) by using your credentials
+(through the GitHub OAuth sign in option) and extract all the code of each programming language of
+every kata you have completed.
+
+**It's discouraged to put your Codewars solutions publicly available because, even though it depends
+on every person and it's a responsibility of one self, having your solutions publicly available may
+increase the number of dishonest users that copy and paste the solutions to their own advantage. An user
+can get sanctioned if the Codewars' staff notice an unusual behaviour. Read the
+[Codewars Code of Conduct](https://docs.codewars.com/community/rules/) for more information (especially
+the last rule).**
+
+**Although it's not prohibited, please: don't put your Codewars solutions publicly available on GitHub.
+Keep your repository private.**
+
+To keep this file simple and short, I only describe here
+the most important information, and a summary of other topics. To see all the information, go to the
+[repository wiki](https://github.com/JoseDeFreitas/CodewarsGitHubLogger/wiki).
+
+## Why?
+
+A key skill every programmer should have is problem solving (algorithms, paradigms, data structures,
+etc.), and Codewars is a very good platform to practice this hability because of its structure, support
+of multiple programming languages, user rank system, easily (with moderation) code challenge creation
+system and social interaction between users (similar code-challenges, comments, votes, and more).
+
+By having a private repository that contains all the code challenges you have completed, you can keep a
+personal registry of all your katas in a more readable way. Moreover, you can allow (temporarily) a recruiter
+to see what you've done, so they will know your strenghts and weaknesses, as well as the programming languages
+you use with ease. You could do all of this manually, but going to all the pages of solutions of all the
+katas you've completed (plus more than one programming language if available) would take you a lot of time
+(and you'd have to update it every time you complete a kata).
+
+This repository allows you to copy in your private repository the code of the completed challanges you've
+done automatically: you don't have to do anything, just run the program and let it do its job. Moreover,
+the files and directories are stored in such a way that it's easy to navigate through them.
+
+## Usage
+
+### Requirements
+
+To use this program you need:
+
+- dotnet core 5.0.
+- A Codewars account.
+- A GitHub account.
+
+Important things to note: if you want to use the program without modifying it and as-is, **you must have
+registered into Codewars using the GitHub OAuth option** and **you must sign in to Codewars before running
+the program** so GitHub won't ask you to confirm the sign in with a code. The Firefox driver can't
+accomplish this because the code is sent to your email address.
+
+If you registered into Codewars using its own registration system instead of the GitHub OAuth method, you
+can change the file to receive and sign in with your Codewars credentials. You can read the subsection
+"[Use Codewars credentials instead of GitHub's](https://github.com/JoseDeFreitas/CodewarsGitHubLogger/wiki/Examples#use-codewars-credentials-instead-of-githubs)" to know how to do this.
+
+### All-in-one method
+
+To use this program you just need an initial configuration. After that, you just have to sit down and watch
+everything being pulled automatically. Below is the ordered list of steps you must follow to get all working:
+
+1. Click on the green button "Use this template".
+2. Choose a name for your repository, mark the "Private" option and click on "Create repository from template".
+3. [Clone](https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository) or [add remotely](https://docs.github.com/en/get-started/getting-started-with-git/managing-remote-repositories) to pull the new repository locally in your machine.
+4. Move to the `CodewarsGitHubLogger` folder.
+5. Run the program by writing `dotnet run -- YOUR_CODEWARS_USERNAME YOUR_GITHUB_USERNAME YOUR_GITHUB_PASSWORD` in your command line.
+   - Optionally you can append the flag `-i` or `--index` as the last argument to create an index file.
+6. Wait for the program to complete (you'll see a message that indicates the completion status).
+7. Add all the files to the stage, commit them and push them to your private repository.
+
+(Optional step): add the folder `CodewarsGitHubLogger` to the `.gitignore` file. This is useful if you
+don't want to see the folder that contains the program I've created, as it's not a kata by itself.
+You can do this without getting into any trouble, because the program is intended to be run locally, so
+there's no need in having it visible on GitHub.
+
+For more information about customisation of the repository and the program, read the
+["Customisation" section](https://github.com/JoseDeFreitas/CodewarsGitHubLogger/wiki/Customisation) of the wiki.
+
+### Other methods
+
+Of course, you can just download the entire repository as a `.zip` file and keep only the
+[CodewarsGitHubLogger](/CodewarsGitHubLogger) folder. Then, make it so it creates and updates
+all the files in the folder you want (the folder that is connected to the private GitHub repository
+of your choice). I provide a [simple example of a local run to a different folder](https://github.com/JoseDeFreitas/CodewarsGitHubLogger/wiki/Examples#create-all-files-in-a-different-directory) in the wiki.
+
+### After completion
+
+And that's it! Keep in mind that it will take some minutes the first time you run it (especially if you
+have completed a lot of katas, because the program must loop through all of them). You must run the program
+every time you want to update the katas. It takes into consideration repeated files and code refactors.
+
+You can customise some aspects of the program, such as the content of the README.md files,
+the name and the directory of the INDEX.md file and other stuff. Head to the ["Customisation" section](https://github.com/JoseDeFreitas/CodewarsGitHubLogger/wiki/Customisation) in
+the wiki of this repository to learn more about what you can do. Also, head to the ["Privacy" page](https://github.com/JoseDeFreitas/CodewarsGitHubLogger/wiki/Privacy) in
+the wiki to learn about the protection of your credentials. I recommend you take a read to the entire
+wiki because there is useful and important information for you to know.
+
+If you're facing a problem, go to the ["Failures" page](https://github.com/JoseDeFreitas/CodewarsGitHubLogger/wiki/Failures)
+of the wiki and look if the error/warning you're getting appears in there. If it doesn't, please open an
+issue by choosing a template from the available [issue templates](https://github.com/JoseDeFreitas/CodewarsGitHubLogger/issues/new/choose).
+
+## Screenshots
+
+<details>
+   <summary>Main repository page</summary>
+   <img src="https://user-images.githubusercontent.com/37962411/168847084-a6d12825-881e-414c-8ae3-0c7655c7a63a.png">
+</details>
+
+<details>
+   <summary>Folders of the katas</summary>
+   <img src="https://user-images.githubusercontent.com/37962411/168847279-53692969-6a12-4b2e-8ed6-85b929d49beb.png">
+</details>
+
+<details>
+   <summary>Inside a kata folder</summary>
+   <img src="https://user-images.githubusercontent.com/37962411/168847303-9f71d056-5888-489d-804d-12c0b330ad91.png">
+</details>
+
+<details>
+   <summary>Solution of a kata</summary>
+   <img src="https://user-images.githubusercontent.com/37962411/168847325-97818ed9-da90-4dda-bb41-cbaa6b04cc2c.png">
+</details>
+
+## Disclaimer
+
+This repository is under the [BSD-2-Clause License](LICENSE) to follow
+[Codewars's Terms of Service](https://www.codewars.com/about/terms-of-service) (that were last updated
+on October, 2018).
+
+If you've read above sections, you know you can customise the program to make it work as you want. Keep in
+mind that **I offer the program as you see in this repository**. Any changes you made in your private repository
+are up to you, and I'm not responsible for them. Be careful on what you change. For more related information,
+please read the ["Privacy" page](https://github.com/JoseDeFreitas/CodewarsGitHubLogger/wiki/Privacy) of the
+wiki. I encourage you to read the whole wiki, too.
